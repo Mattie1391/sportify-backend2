3a0b232c2421a481973eebf8fd50d8ea66dbbb94
Mattie <78979239+Mattie1391@users.noreply.github.com>
2025-04-24 22:52:01 +0800

    Initial commit

---
+++ config/data-source.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const User = require('../entities/User');
+
+const { DataSource, useContainer } = require('typeorm');
+
+
+const AppDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [User],
+  synchronize: true,
+  logging: false
+});
+
+module.exports = { AppDataSource };
+
---
+++ entities/User.js
@@ -0,0 +1,17 @@
+// models/User.js
+const { EntitySchema } = require('typeorm');
+
+module.exports = new EntitySchema({
+  name: "User",
+  tableName: "users",
+  columns: {
+    id: {
+      primary: true,
+      type: "int",
+      generated: true,
+    },
+    name: {
+      type: "varchar",
+    },
+  },
+});
